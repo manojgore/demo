f44ad8dd4d2df8028350b5a917cd33d868780cc5
MANOJ GORE <manojgore.mg@gmail.com>
2024-02-09 10:15:58 +0530

    logo route change

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -97,7 +97,7 @@
 						<span class="icon-bar"></span>
 					</button>
         </div>
-        <a href="index.html" class="navbar_brand float-start dn-md">
+        <a href="{{route('HomePage')}}" class="navbar_brand float-start dn-md">
           <img class="logo1 img-fluid" src="images/header-logo.svg" alt="header-logo.svg">
           <img class="logo2 img-fluid" src="images/header-logo2.svg" alt="header-logo2.svg">
 				</a>
